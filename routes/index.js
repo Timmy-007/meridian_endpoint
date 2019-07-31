2983cc719846d8343c4c17c4f58e5a8c0d402316
Tijssens <ttijssens@hpe.com>
2019-07-31 10:33:14 +0200

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var postVariable="Express";
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: postVariable });
+});
+//router.get('/meridian/', function (req, res) {
+//    postVariable = req.body;
+//    res.render('index', { title: postVariable });
+//});
+router.post('/meridian/', function (req, res) {
+   
+    console.log('world');
+    postVariable = req.body.campaign_type;
+    
+    res.render('index', { title: postVariable });
+});
+
+
+
+module.exports = router;
